7a5e61c295cdb5772f4002311a3de29e116637dd
Wesley Gomes Queiroz <wgomesq@santillana.local>
2022-12-27 19:24:23 -0300

    firt commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dark-light';
+  theme: Theme = 'light-theme';
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2
+  ) { }
+
+  ngOnInit(): void {
+    this.initializeTheme();
+  }
+
+  initializeTheme = (): void =>
+    this.renderer.addClass(this.document.body, this.theme);
+
+  switcTheme() {
+    this.document.body.classList.replace(
+      this.theme,
+      this.theme === 'light-theme'
+        ? (this.theme = 'dark-theme')
+        : (this.theme = 'light-theme')
+    );
+  }
 }
+
+export type Theme = 'light-theme' | 'dark-theme';
